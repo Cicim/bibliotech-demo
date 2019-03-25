1343de6346a7db2915eba842fdb93c55b960c633
Claudio Cicimurri <claudio.ccmrr@gmail.com>
2019-03-25 14:23:56 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+
+    <head>
+        <title>Vetrina - Bibliotek@</title>
+        <?php
+            // Inserisci tutti gli include
+            include 'utils/imports.php';
+        ?>
+    </head>
+
+    <body>
+        <?php 
+            // Includi l'header nella pagina
+            include 'utils/header.php'
+
+        ?>
+    </body>
+</html>
---
+++ utils/imports.php
@@ -0,0 +1,7 @@
+<script src="/biblioteca/scripts/jquery-3.3.1.min.js"></script>
+<script src="/biblioteca/scripts/bootstrap.bundle.min.js"></script>
+<script src="/biblioteca/scripts/popper.min.js"></script>
+<script src="/biblioteca/scripts/bootstrap.js"></script>
+
+
+<link rel="stylesheet" href="/biblioteca/styles/global.css">
